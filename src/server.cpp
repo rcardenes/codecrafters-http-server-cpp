df3a61f155f50659ee650929a35074af2bef5268
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2024-02-02 18:28:34 -0300

    Separate responsabilities a bit and modernize the C++

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,6 @@
+#include <string_view>
+#include <expected>
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -8,46 +11,78 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argc, char **argv) {
+using namespace std::literals;
+
+constexpr auto VANILLA_200_OK_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n"sv;
+constexpr int DEFAULT_PORT = 4221;
+
+enum class socket_error {
+    cant_create_socket,
+    setsockopt,
+    cant_bind,
+    cant_listen,
+};
+
+auto setup_server_socket() -> std::expected<int, socket_error>
+{
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+   return std::unexpected(socket_error::cant_create_socket);
   }
 
   // Since the tester restarts your program quite often, setting REUSE_PORT
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
+    return std::unexpected(socket_error::setsockopt);
   }
 
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
+  server_addr.sin_port = htons(DEFAULT_PORT);
 
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
-    return 1;
+      return std::unexpected(socket_error::cant_bind);
   }
 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
+      return std::unexpected(socket_error::cant_listen);
   }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
+  return server_fd;
+}
 
-  std::cout << "Waiting for a client to connect...\n";
+int main(int argc, char **argv) {
+  if (const auto server = setup_server_socket(); server.has_value()) {
+      // Serve...
+      auto server_fd = *server;
 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+      struct sockaddr_in client_addr;
+      int client_addr_len = sizeof(client_addr);
 
-  close(server_fd);
+      std::cout << "Waiting for a client to connect...\n"sv;
 
+      accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+      std::cout << "Client connected\n"sv;
+
+      close(server_fd);
+  } else {
+      switch (server.error()) {
+          case socket_error::cant_create_socket:
+              std::cerr << "Failed to create server socket\n"sv;
+              break;
+          case socket_error::setsockopt:
+              std::cerr << "setsockopt failed\n"sv;
+              break;
+          case socket_error::cant_bind:
+              std::cerr << "Failed to bind to port "sv << DEFAULT_PORT << '\n';
+              break;
+          case socket_error::cant_listen:
+              std::cerr << "listen failed\n"sv;
+              break;
+      }
+  }
   return 0;
 }
